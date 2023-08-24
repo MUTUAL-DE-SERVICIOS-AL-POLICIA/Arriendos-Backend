b7ef0eed424532cd6ce2d229136271322d145af3
gongor7 <crisgongor7@gmail.com>
2023-08-24 16:07:12 -0400

    Cambio de nombre de clases

--- products/views.py
+++ products/views.py
@@ -2,27 +2,27 @@ from rest_framework import generics
 from .models import Rate, Time, Product
 from .serializers import RateSerializer, TimeSerializer, ProductSerializer
 
-class RateListCreateView(generics.ListCreateAPIView):
+class Rate_List_Create_View(generics.ListCreateAPIView):
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
 
-class RateRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class Rate_Retrieve_Update_Destroy_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
 
-class TimeListCreateView(generics.ListCreateAPIView):
+class Time_List_Create_View(generics.ListCreateAPIView):
     queryset = Time.objects.all()
     serializer_class = TimeSerializer
 
-class TimeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class Time_Retrieve_Update_Destroy_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = Time.objects.all()
     serializer_class = TimeSerializer
 
 
-class ProductListCreateView(generics.ListCreateAPIView):
+class Product_List_Create_View(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class Product_Retrieve_Update_Destroy_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
