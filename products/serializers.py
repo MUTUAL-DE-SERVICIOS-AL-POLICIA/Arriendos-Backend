8533cfa2455030c24bd78c60b9c4006cd7da6293
gongor7 <crisgongor7@gmail.com>
2023-08-24 15:36:41 -0400

    Creación de vistas y CRUD para productos

---
+++ products/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Rate, Time, Day, Product
+
+class RateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rate
+        fields = '__all__'
+
+class TimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Time
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
