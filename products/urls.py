d317368ad8608cdfdb28f9dccb6598bb86fe42c2
gongor7 <crisgongor7@gmail.com>
2023-08-24 16:07:12 -0400

    Cambio de nombre de clases

--- products/urls.py
+++ products/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from .views import (
-    RateListCreateView, RateRetrieveUpdateDestroyView,
-    TimeListCreateView, TimeRetrieveUpdateDestroyView,
-    ProductListCreateView, ProductRetrieveUpdateDestroyView
+    Rate_List_Create_View, Rate_Retrieve_Update_Destroy_View,
+    Time_List_Create_View, Time_Retrieve_Update_Destroy_View,
+    Product_List_Create_View, Product_Retrieve_Update_Destroy_View
 )
 
 urlpatterns = [
-    path('', ProductListCreateView.as_view(), name='product-list-create'),
-    path('<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-retrieve-update-destroy'),
+    path('', Product_List_Create_View.as_view(), name='product-list-create'),
+    path('<int:pk>/', Product_Retrieve_Update_Destroy_View.as_view(), name='product-retrieve-update-destroy'),
 
-    path('rates/', RateListCreateView.as_view(), name='rate-list-create'),
-    path('rates/<int:pk>/', RateRetrieveUpdateDestroyView.as_view(), name='rate-retrieve-update-destroy'),
+    path('rates/', Rate_List_Create_View.as_view(), name='rate-list-create'),
+    path('rates/<int:pk>/', Rate_Retrieve_Update_Destroy_View.as_view(), name='rate-retrieve-update-destroy'),
 
-    path('times/', TimeListCreateView.as_view(), name='time-list-create'),
-    path('times/<int:pk>/', TimeRetrieveUpdateDestroyView.as_view(), name='time-retrieve-update-destroy'),
+    path('times/', Time_List_Create_View.as_view(), name='time-list-create'),
+    path('times/<int:pk>/', Time_Retrieve_Update_Destroy_View.as_view(), name='time-retrieve-update-destroy'),
 ]
