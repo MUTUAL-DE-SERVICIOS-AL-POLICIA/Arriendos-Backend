e5c0f64659d820f6245fd04600e45b036c694325
KevinCondori <kevin.condori699@gmail.com>
2023-09-25 17:02:35 -0400

    API usuarios url

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from users.views import User_Ldap, Assign_Api, Assign_Detail, User_Delete
 
 urlpatterns = [
     path('', User_Ldap.as_view()),
-    path('active/<str:pk>', User_Delete.as_view()),
+    path('state/<str:pk>', User_Delete.as_view()),
     path('get_user/',views.get_user),
     path('assign/', Assign_Api.as_view()),
     path('assign/<str:pk>', Assign_Detail.as_view()),
