6234d336c492a56a79b5c2160e21eb36e5622b39
KevinCondori <kevin.condori699@gmail.com>
2023-10-02 10:04:18 -0400

    API clientes y contactos segun la tarifa y tipo cliente

--- customers/models.py
+++ customers/models.py
@@ -4,15 +4,21 @@ class Customer_type(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     is_institution = models.BooleanField(default=True)
-class Institution (models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_name = models.CharField(max_length=100)
-    ci = models.CharField(max_length=30)
-    phone = models.CharField(max_length=100)
-    grade = models.CharField(max_length=100,null=True)
     customer_type = models.ForeignKey(Customer_type, related_name="customer_types", on_delete=models.CASCADE)
-    institution= models.ForeignKey(Institution, related_name="Institution",null=True,on_delete=models.CASCADE)
+    institution_name = models.CharField(null=True)
+    nit = models.CharField(max_length=50, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Contact(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    degree = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=100)
+    ci_nit = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=50)
+    is_customer = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
