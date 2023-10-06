2f4b4f85f038b423a336128bfec6929752fa2df3
gongor7 <crisgongor7@gmail.com>
2023-10-06 12:26:09 -0400

    refactorización de modelo rango de hora

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='HourRange',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=250)),
+                ('time', models.CharField(max_length=250)),
             ],
         ),
         migrations.CreateModel(
