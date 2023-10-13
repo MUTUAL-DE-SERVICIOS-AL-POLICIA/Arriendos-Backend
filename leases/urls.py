c8295a92477385074d4ef099dd895ef80592a616
gongor7 <crisgongor7@gmail.com>
2023-10-13 18:41:28 -0400

    API para obtener el estado del arriendo y su siguiente posible con validacion de parametro

--- leases/urls.py
+++ leases/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 from leases import views
-from leases.views import Selected_Product_Api, Event_Api,Change_state
+from leases.views import Selected_Product_Api, Event_Api,Get_state
 urlpatterns = [
     path('', Selected_Product_Api.as_view()),
     path('event/', Event_Api.as_view()),
-    path('get_state/', Change_state.as_view())
+    path('get_state/', Get_state.as_view())
  ]
