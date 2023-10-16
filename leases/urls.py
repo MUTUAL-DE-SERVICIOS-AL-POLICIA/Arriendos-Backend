968f4652223fd24b0fc07b258753c94a583e6112
gongor7 <crisgongor7@gmail.com>
2023-10-16 18:24:50 -0400

    APi para listar todos los estados de arriendo

--- leases/urls.py
+++ leases/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
 from leases import views
-from leases.views import Selected_Product_Api, Event_Api,Get_state
+from leases.views import Selected_Product_Api, Event_Api,Get_state, StateRentalListCreateView
 urlpatterns = [
     path('', Selected_Product_Api.as_view()),
     path('event/', Event_Api.as_view()),
-    path('get_state/', Get_state.as_view())
+    path('get_state/', Get_state.as_view()),
+    path('state/',StateRentalListCreateView.as_view())
  ]
