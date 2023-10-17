43c4fcab075b279aa3002cfc09809ec05689abac
KevinCondori <kevin.condori699@gmail.com>
2023-10-17 16:11:25 -0400

    API Selected products con precios y total inicial

--- leases/serializer.py
+++ leases/serializer.py
@@ -1,16 +1,14 @@
 from rest_framework import serializers
 from .models import State, Selected_Product, Rental, Event_Type
-from products.models import Rate
 from products.serializers import RateSerializer
-from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from products.models import Product
-from customers.serializer import CustomerSerializer
+
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
         fields = '__all__'
-        
+
 
 class RentalSerializer(serializers.ModelSerializer):
     class Meta:
@@ -33,7 +31,7 @@ class Selected_ProductSerializer(serializers.ModelSerializer):
      product = ProductSerializer()
      event_type = Event_TypeSerializer()
      rental = RentalSerializer()
-     
+
      class Meta:
         model = Selected_Product
         fields = '__all__'
--- leases/urls.py
+++ leases/urls.py
@@ -1,11 +1,10 @@
 from django.urls import include, path
-from rest_framework import routers
-from leases import views
-from leases.views import Selected_Product_Api, Event_Api,Get_state, StateRentalListCreateView, List_state
+from leases.views import Pre_Booking_Api, Event_Api,Get_state, StateRentalListCreateView, List_state
+
 urlpatterns = [
-    path('', Selected_Product_Api.as_view()),
+    path('', Pre_Booking_Api.as_view()),
     path('event/', Event_Api.as_view()),
     path('get_state/', Get_state.as_view()),
     path('state/',StateRentalListCreateView.as_view()),
     path('list_state/',List_state.as_view()),
- ]
+    ]
