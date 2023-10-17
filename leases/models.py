976ff3f479d84e09f3c504c9a50deb6e65cb59c3
KevinCondori <kevin.condori699@gmail.com>
2023-10-17 12:26:05 -0400

    Reestructuración de modelo y migración de Rental

--- leases/models.py
+++ leases/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from customers.models import Customer
 from products.models import Product
+from plans.models import Plan
 from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class State(models.Model):
@@ -11,8 +12,11 @@ class Rental(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     is_plan=models.BooleanField(default=False)
+    plan = models.ForeignKey(Plan, null=True, on_delete=models.CASCADE)
+    initial_total = models.FloatField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 class Event_Type(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,6 +29,7 @@ class Selected_Product(models.Model):
     start_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
     detail = models.CharField(null=True)
+    product_price = models.FloatField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
