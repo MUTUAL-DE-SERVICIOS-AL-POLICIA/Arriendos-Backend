d41577dd2d544badda52a380fae497854fc88632
KevinCondori <kevin.condori699@gmail.com>
2023-10-17 17:51:55 -0400

    Api Leases rutas

--- leases/urls.py
+++ leases/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from leases.views import Pre_Booking_Api, Event_Api,Get_state, StateRentalListCreateView, List_state
+from leases.views import Pre_Reserve_Api, Selected_Product_Calendar_Api, Event_Api,Get_state, StateRentalListCreateView, List_state
 
 urlpatterns = [
-    path('', Pre_Booking_Api.as_view()),
+    path('', Pre_Reserve_Api.as_view()),
+    path('calendar/', Selected_Product_Calendar_Api.as_view()),
     path('event/', Event_Api.as_view()),
     path('get_state/', Get_state.as_view()),
     path('state/',StateRentalListCreateView.as_view()),
