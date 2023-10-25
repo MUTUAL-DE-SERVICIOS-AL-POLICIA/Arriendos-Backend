2d0f0975a6f6862339540e40732e6900d5113ad6
KevinCondori <kevin.condori699@gmail.com>
2023-10-25 09:10:35 -0400

    API Rental edición de fechas de los eventos y validación de fechas

--- leases/serializer.py
+++ leases/serializer.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import State, Selected_Product, Rental, Event_Type
 from products.serializers import RateSerializer
 from rooms.serializers import RoomSerializer
-from products.models import Product
+from products.models import Product, HourRange
+from products.serializers import HourRangeSerializer
+from customers.serializer import CustomersSerializer
+import pytz
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,10 +26,16 @@ class Event_TypeSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     room = RoomSerializer()
     rate = RateSerializer()
+    hour_range = HourRangeSerializer()
 
     class Meta:
         model = Product
         fields = '__all__'
+    def get_hour_range(self, obj):
+        hour_ranges = HourRange.objects.filter(id=obj)
+        hour_ranges_serializer = HourRangeSerializer(hour_ranges, many = True)
+        return hour_ranges_serializer.data
+
 class Selected_ProductSerializer(serializers.ModelSerializer):
      product = ProductSerializer()
      event_type = Event_TypeSerializer()
@@ -35,3 +44,63 @@ class Selected_ProductSerializer(serializers.ModelSerializer):
      class Meta:
         model = Selected_Product
         fields = '__all__'
+
+month_spanish = {
+    'January': 'Enero',
+    'February': 'Febrero',
+    'March': 'Marzo',
+    'April': 'Abril',
+    'May': 'Mayo',
+    'June': 'Junio',
+    'July': 'Julio',
+    'August': 'Agosto',
+    'September': 'Septiembre',
+    'October': 'Octubre',
+    'November': 'Noviembre',
+    'December': 'Diciembre',
+}
+
+class CustomDateTimeField(serializers.DateTimeField):
+    def to_representation(self, value):
+        time_zone = pytz.timezone('America/La_Paz')
+        normalized_date = value.astimezone(time_zone)
+        month = month_spanish[value.strftime('%B')]
+        formatted_date = normalized_date.strftime('%d de ') + month + normalized_date.strftime(' de %Y %H:%M %p')
+        return formatted_date
+
+    def get_hour_only(self, value):
+            time_zone = pytz.timezone('America/La_Paz')
+            normalized_date = value.astimezone(time_zone)
+            hour_only = normalized_date.strftime('%H:%M %p')
+            return hour_only
+
+class Selected_ProductsSerializer(serializers.ModelSerializer):
+     product = ProductSerializer()
+     event_type = Event_TypeSerializer()
+     start_time = CustomDateTimeField(format="%d de %B de %Y %H:%M %p")
+     end_time = CustomDateTimeField(format="%d de %B de %Y %H:%M %p")
+     start_time_only_hour = serializers.SerializerMethodField()
+     end_time_only_hour = serializers.SerializerMethodField()
+
+     class Meta:
+        model = Selected_Product
+        fields =  ('id', 'detail', 'event_type', 'start_time', 'end_time', 'product', 'rental', 'product_price', 'start_time_only_hour', 'end_time_only_hour')
+
+     def get_start_time_only_hour(self, obj):
+        return CustomDateTimeField().get_hour_only(obj.start_time)
+
+     def get_end_time_only_hour(self, obj):
+        return CustomDateTimeField().get_hour_only(obj.end_time)
+
+class RentalsSerializer(serializers.ModelSerializer):
+    customer = CustomersSerializer()
+    state = StateSerializer()
+    selected_products = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Rental
+        fields = '__all__'
+    def get_selected_products(self, obj):
+        selected_products = Selected_Product.objects.filter(rental_id = obj)
+        selected_product_serializer = Selected_ProductsSerializer(selected_products, many=True)
+        return selected_product_serializer.data
