c06847aeafba18b873f04ea6935a25120405e21d
gongor7 <crisgongor7@gmail.com>
2023-10-25 14:59:12 -0400

    API para registrar montos de ingreso de garantías

--- financials/serializer.py
+++ financials/serializer.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from financials.models import Payment
+from financials.models import Payment, Warranty_Movement
+
 class Payment_Serializer(serializers.ModelSerializer):
     class Meta:
         model=Payment
         fields = '__all__'
+
+class Warranty_Movement_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model=Warranty_Movement
+        fields ='__all__'
