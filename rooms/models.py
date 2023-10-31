c48a985e2bf97cc1770692040b98d327e0df1b6d
gongor7 <crisgongor7@gmail.com>
2023-10-31 15:45:12 -0400

    API para obtener el precio de hora extra por producto seleccionado

--- rooms/models.py
+++ rooms/models.py
@@ -15,8 +15,7 @@ class Room(models.Model):
     warranty=models.FloatField()
     is_active = models.BooleanField(default=True)
     group = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
+
 
 class Sub_Environment(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
