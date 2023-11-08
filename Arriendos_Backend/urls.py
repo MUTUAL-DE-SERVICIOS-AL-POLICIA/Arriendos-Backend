0b584693778fae352ce61d360c827f1e8f23b18a
KevinCondori <kevin.condori699@gmail.com>
2023-11-08 15:37:11 -0400

    Instalación de DRF Spectacular

--- Arriendos_Backend/urls.py
+++ Arriendos_Backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +31,7 @@ urlpatterns = [
     path('api/requirements/', include('requirements.urls')),
     path('api/leases/', include('leases.urls')),
     path('api/financials/', include('financials.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
