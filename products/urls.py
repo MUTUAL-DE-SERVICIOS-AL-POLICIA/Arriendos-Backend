b9c721f7ce985ab63ceec582616d1bf0359bc5ef
KevinCondori <kevin.condori699@gmail.com>
2023-11-10 16:15:52 -0400

    Adición de Swagger a la aplicación Products

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    Rate_Api, Rate_Detail,
+    Rate_Api,
     HourRange_List_Create_View, HourRange_Retrieve_Update_Destroy_View,
     Product_Api,
     Price_List_Create_View,
@@ -16,7 +16,6 @@ urlpatterns = [
     path('<int:pk>', Product_Api.as_view(), name='product-retrieve-update-destroy'),
 
     path('rates/', Rate_Api.as_view(), name='rate-list-create'),
-    path('rates/<int:pk>', Rate_Detail.as_view()),
 
     path('hour-range/', HourRange_List_Create_View.as_view(), name='time-list-create'),
     path('hour-range/<int:pk>', HourRange_Retrieve_Update_Destroy_View.as_view(), name='range-retrieve-update-destroy'),
