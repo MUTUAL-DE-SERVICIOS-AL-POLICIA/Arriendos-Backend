c0ac87ca1196b6ff02db8f6aeb8236915ab6e299
gongor7 <crisgongor7@gmail.com>
2023-11-13 12:34:39 -0400

    Adición de Permisos para API's de CRUD de productos

--- products/permissions.py
+++ products/permissions.py
@@ -6,3 +6,19 @@ class HasViewRatePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         has_permission = request.user.has_perm('products.view_rate')
         return has_permission
+
+class HasViewProductPermission(permissions.BasePermission):
+    message = "No tienes permisos para ver los productos"
+    def has_permission(self, request, view):
+        has_permission = request.user.has_perm('products.view_product')
+        return has_permission
+class HasAddroductPermission(permissions.BasePermission):
+    message = "No tienes permisos para crear productos"
+    def has_permission(self, request, view):
+        has_permission = request.user.has_perm('products.add_product')
+        return has_permission
+class HasChangeProductPermission(permissions.BasePermission):
+    message = "No tienes permisos para editar productos"
+    def has_permission(self, request, view):
+        has_permission = request.user.has_perm('products.change_product')
+        return has_permission
