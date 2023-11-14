0df3c357cc58dcbec74bde4e74531ee9d208f445
KevinCondori <kevin.condori699@gmail.com>
2023-11-14 09:47:18 -0400

    Adición de Swagger a Login y Users

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
-from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
 from login import views
+from .views import Users_Ldap
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,8 +8,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("connect_ldap/", views.Connect_Ldap),
-    path("search_ldap/", views.Search_User_Ldap),
     path('auth/', views.Auth.as_view(), name='token_obtain_pair'),
-    path('users_ldap/', views.Users_ldap),
+    path('users_ldap/', Users_Ldap.as_view()),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
