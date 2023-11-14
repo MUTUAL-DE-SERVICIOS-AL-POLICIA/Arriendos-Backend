9478cc6ea6e37d41d3ade360acbca47a27d6fb3a
KevinCondori <kevin.condori699@gmail.com>
2023-11-14 09:47:18 -0400

    Adición de Swagger a Login y Users

--- login/views.py
+++ login/views.py
@@ -4,24 +4,21 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from ldap3 import Server, Connection, ALL, SUBTREE, ALL_ATTRIBUTES
-from ldap3.core.exceptions import LDAPException
+from ldap3 import Server, Connection, ALL, SUBTREE
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import status
+from rest_framework import status, generics
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from users.serializers import UserCustomSerializer
-from ldap3.abstract.attribute import Attribute
 from django.contrib.auth.hashers import make_password
-import json
+from drf_yasg.utils import swagger_auto_schema
 
 def Bind_User_Ldap(user, password):
-    ldap_server = settings.LDAP_SERVER 
+    ldap_server = settings.LDAP_SERVER
     ldap_user = settings.LDAP_USER
     ldap_password = settings.LDAP_PASSWORD
     server = Server(ldap_server, get_info=ALL)
-    connection = Connection(server, ldap_user, ldap_password, auto_bind=True)  
+    connection = Connection(server, ldap_user, ldap_password, auto_bind=True)
     user_dn = f"uid={user}, {settings.LDAP_USER_DN}"
     try:
         with Connection(server, user_dn, password, auto_bind=True):
@@ -30,7 +27,7 @@ def Bind_User_Ldap(user, password):
         return None
 
 def Connect_Ldap(request):
-    ldap_server = settings.LDAP_SERVER 
+    ldap_server = settings.LDAP_SERVER
     ldap_user = settings.LDAP_USER
     ldap_password = settings.LDAP_PASSWORD
     server = Server(ldap_server, get_info=ALL)
@@ -39,19 +36,11 @@ def Connect_Ldap(request):
         with connection:
             return JsonResponse({"estado":"conectado"}, status=202)
     except:
-        return JsonResponse({"estado":"falló la conexión"}, status=404)
-@csrf_exempt
-@api_view(['POST'])
-def Search_User_Ldap(request):
-    user=request.data.get('user')
-    password=request.data.get('password')
-    if Bind_User_Ldap(user, password)==True: 
-        return HttpResponse("usuario válido")
-    else:
-        return HttpResponse("no coincide el usuario y contraseña")
+        return JsonResponse({"error":"falló la conexión"}, status=404)
 
 User = get_user_model()
 class Auth(TokenObtainPairView):
+
     def post(self, request, *args, **kwargs):
         user=request.data.get('username')
         password=request.data.get('password')
@@ -61,7 +50,7 @@ class Auth(TokenObtainPairView):
                 user.password = make_password(password)
                 user.save()
                 response = super().post(request, *args, **kwargs)
-                if response.status_code == status.HTTP_200_OK:  
+                if response.status_code == status.HTTP_200_OK:
                     user = User.objects.filter(username=request.data['username']).first()
                     response.data['user_id'] = user.id
                     response.data['username'] = user.username
@@ -71,7 +60,7 @@ class Auth(TokenObtainPairView):
                         response.data['detail'] = "Credenciales inválidas"
                         response.status_code = status.HTTP_401_UNAUTHORIZED
             else:
-                 return Response({'detail': 'Credenciales LDAP inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
+                 return Response({'error': 'Credenciales LDAP inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             response = super().post(request, *args, **kwargs)
             if response.status_code == status.HTTP_200_OK:
@@ -85,28 +74,32 @@ class Auth(TokenObtainPairView):
                     response.status_code = status.HTTP_401_UNAUTHORIZED
         return response
 
-def Users_ldap(request):
-    serializer_class = UserCustomSerializer
-    queryset = User.objects.all()
-    ldap_server = settings.LDAP_SERVER
-    ldap_password = settings.LDAP_PASSWORD
-    server = Server(ldap_server, get_info=ALL)
-    connection = Connection(server, settings.LDAP_USER, ldap_password, auto_bind=True) 
-    connection.search(settings.LDAP_BASE, settings.LDAP_FILTER, SUBTREE, attributes=settings.ATTRIBUTES)
-    data = []
-    users_dba = User.objects.all()
-    user_to_compare = []
-    for user_dba in users_dba:
-        user_list = user_dba.username
-        user_to_compare.append(user_list)
-    for entry in connection.entries:
-        if not entry.uid in user_to_compare:
-            user = {
-                    "username": f"{entry.uid}",
-                    "first_name": f"{entry.givenName}",
-                    "last_name": f"{entry.sn}",
-                    "email": f"{entry.mail}"
-                    }
-            data.append(user)
-    response_data = {"message": "List of users LDAP", "users": data}
-    return JsonResponse(response_data, safe=False, status=status.HTTP_202_ACCEPTED)
+class Users_Ldap(generics.GenericAPIView):
+    @swagger_auto_schema(
+    operation_description="Listado de usuarios LDAP",
+    )
+    def get(self, request, *args, **kwargs):
+        serializer_class = UserCustomSerializer
+        queryset = User.objects.all()
+        ldap_server = settings.LDAP_SERVER
+        ldap_password = settings.LDAP_PASSWORD
+        server = Server(ldap_server, get_info=ALL)
+        connection = Connection(server, settings.LDAP_USER, ldap_password, auto_bind=True)
+        connection.search(settings.LDAP_BASE, settings.LDAP_FILTER, SUBTREE, attributes=settings.ATTRIBUTES)
+        data = []
+        users_dba = User.objects.all()
+        user_to_compare = []
+        for user_dba in users_dba:
+            user_list = user_dba.username
+            user_to_compare.append(user_list)
+        for entry in connection.entries:
+            if not entry.uid in user_to_compare:
+                user = {
+                        "username": f"{entry.uid}",
+                        "first_name": f"{entry.givenName}",
+                        "last_name": f"{entry.sn}",
+                        "email": f"{entry.mail}"
+                        }
+                data.append(user)
+        response_data = {"message": "List of users LDAP", "users": data}
+        return JsonResponse(response_data, safe=False, status=status.HTTP_202_ACCEPTED)
