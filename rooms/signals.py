e64d23c7bfdd6be7785760b99d21ca3a9b968fd3
gongor7 <crisgongor7@gmail.com>
2023-11-16 11:15:48 -0400

    Cración de Registro de records para el modelo Room

---
+++ rooms/signals.py
@@ -0,0 +1,47 @@
+from django.db.models.signals import post_save, pre_save, post_delete
+from django.dispatch import receiver
+from users.models import Record
+from threadlocals.threadlocals import get_thread_variable
+from .models import *
+
+@receiver(post_save, sender=Room)
+def log_create_room(sender, instance, created, **kwargs):
+    model="Room"
+    user = get_thread_variable('thread_user')
+    if created:
+        detail=f"El usuario: {user} creó el registro {instance}"
+        action="create"
+        Record.objects.create(
+            user=user,
+            action=action,
+            model=model,
+            detail=detail
+        )
+@receiver(pre_save, sender=Room)
+def log_edit_user(sender, instance, **kwargs):
+    action="update"
+    model="Room"
+    if instance.pk is not None:
+        old_instance = Room.objects.get(pk=instance.pk)
+        for field in Room._meta.fields:
+            old_value = getattr(old_instance, field.name)
+            new_value = getattr(instance, field.name)
+            user = get_thread_variable('thread_user')
+            if old_value != new_value:
+                Record.objects.create(
+                    user=user,
+                    action=action,
+                    model=model,
+                    detail=f'El usuario: {user} realizó un cambió en el campo {field.name}: del anterior valor: {old_value}, al nuevo valor: {new_value} del registro: {instance}'
+                )
+@receiver(post_delete, sender=Room)
+def log_delete_room(sender, instance, **kwargs):
+    model="Room"
+    user = get_thread_variable('thread_user')
+    action="delete"
+    Record.objects.create(
+        user=user,
+        action=action,
+        model=model,
+        detail=f"El usuario: {user} eliminó el registro {instance}"
+    )
