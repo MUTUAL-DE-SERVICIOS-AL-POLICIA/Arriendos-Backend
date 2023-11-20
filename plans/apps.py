1869bdfaf8c4987c2476f4cc16c2203f77194c91
KevinCondori <kevin.condori699@gmail.com>
2023-11-20 09:52:09 -0400

    Adición de records a Plans

--- plans/apps.py
+++ plans/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PlansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'plans'
+    def ready(self):
+        from plans import signals
