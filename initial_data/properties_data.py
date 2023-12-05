cd5202a8f332ff285dedaf2509d80dc97838d1b0
gongor7 <crisgongor7@gmail.com>
2023-12-05 09:51:26 -0400

    Actualización de carga de datos de Propiedades

--- initial_data/properties_data.py
+++ initial_data/properties_data.py
@@ -19,15 +19,17 @@ requests_data = [
         "name": "GRAN HOTEL PARIS",
         "address":"La Paz, Plaza Murillo Esq.Bolivar y Ballivian N°1179",
         "photo": ("GRAN_HOTEL_PARIS.jpeg", open(os.path.join(media_directory, "property_photos/GRAN_HOTEL_PARIS.jpeg"), "rb")),
+        "department": "LA PAZ"
     },
     {
         "name": "CLUB POLICIAL LOS OLIVOS",
         "photo": ("CLUB_POLICIAL_LOS_OLIVOS.jpg", open(os.path.join(media_directory, "property_photos/CLUB_POLICIAL_LOS_OLIVOS.jpg"), "rb")),
-        "address": "La Paz, Calle Los Cedros N° 180"
+        "address": "La Paz, Calle Los Cedros N° 180",
+        "department":"LA PAZ"
     }
 ]
 
 for data in requests_data:
     headers = {'Authorization': f'Bearer {token}'}
-    response = requests.post(url, data={"name": data["name"],"address":data["address"]}, files={"photo": data["photo"]}, headers=headers)
+    response = requests.post(url, data={"name": data["name"],"address":data["address"], "department":data["department"]}, files={"photo": data["photo"]}, headers=headers)
     print(response.status_code, response.json())
