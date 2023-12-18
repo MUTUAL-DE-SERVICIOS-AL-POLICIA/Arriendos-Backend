189e82f558408e2f964173455a3e3357eb6963df
gongor7 <crisgongor7@gmail.com>
2023-12-18 16:37:55 -0400

    API para editar garantías

--- financials/urls.py
+++ financials/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from financials.views import Register_payment,Register_warranty,Discount_warranty, Warranty_Returned, Register_total_payment, Warranty_Return_Request, Return_Warranty_Form, Edit_payment
+from financials.views import Register_payment,Register_warranty,Discount_warranty, Warranty_Returned, Register_total_payment, Warranty_Return_Request, Return_Warranty_Form, Edit_payment, Edit_warranty
 
 urlpatterns = [
     path('register_payment/',Register_payment.as_view()),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("register_total_payment/", Register_total_payment.as_view()),
     path('register_warranty/',Register_warranty.as_view()),
     path("register_warranty/<int:rental_id>/", Register_warranty.as_view()),
+    path('edit_warranty/<int:pk>/', Edit_warranty.as_view(), name='warranty-patch'),
     path('discount_warranty/',Discount_warranty.as_view()),
     path('warranty_returned/',Warranty_Returned.as_view()),
     path('warranty_request/', Warranty_Return_Request.as_view()),
